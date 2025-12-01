e699275e17ad25c27dc6501414573271f0831e37
K256 <95189223+K2568@users.noreply.github.com>
2025-12-01 10:16:07 +0000

    Fix file

--- print-all-events/startup.lua
+++ print-all-events/startup.lua
@@ -2,6 +2,6 @@ local count = 0
 
 while True do
     local event = os.pullEventRaw()
-	count = count + 1
-	print(count .. " - " .. event)
+    count = count + 1
+    print(count .. " - " .. event)
 end
